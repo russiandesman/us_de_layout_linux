d315b6ae78b50a178fcec382e95dd09dd23cbf9c
Denis Maryin <des.maryin@googlemail.com>
2025-11-12 16:01:23 +0100

    Add install script
    Signed-off-by: Denis Maryin <des.maryin@googlemail.com>

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ You have a laptop with US keyboard layout and need a way to type the really need
 
 Here is the simple solution for Linux.
 
-Download and run setup.sh script as root
+Download and run install.sh script as root
+
+Alternatively download the debian package from [Releases](https://github.com/russiandesman/us_de_layout_linux/releases) page
 
 Now select "**en(US+DE) RAlt for l3**" from the list of layouts in your keyboard layout selection tool (xfce4-keyboard-settings in XFCE for example)
 
---
+++ install.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+layouts_file="/usr/share/X11/xkb/rules/evdev.xml"
+
+export insert_before_string="</layoutList>"
+export layout_description="
+    <layout>
+      <configItem>
+        <name>us-de</name>
+        <shortDescription>en(US+DE)</shortDescription>
+        <description>en(US+DE) RAlt for l3</description>
+        <languageList>
+          <iso639Id>eng</iso639Id>
+          <iso639Id>deu</iso639Id>
+        </languageList>
+      </configItem>
+      <variantList/>
+    </layout>
+"
+layout_id_string="<name>us-de</name>"
+
+# if not already inserted
+if ! grep -q "${layout_id_string}" ${layouts_file}; then
+	perl -i.backup -pe 'print "$ENV{layout_description}\n" if m|$ENV{insert_before_string}|' ${layouts_file}
+fi
+
